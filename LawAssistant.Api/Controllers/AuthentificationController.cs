6781850a7567174ca664191e3bda16f8c35d7804
VladimirPimenov <pimenovv2000@gmail.com>
2026-03-28 02:35:19 +0300

    Добавлен logout

--- LawAssistant.Api/Controllers/AuthentificationController.cs
+++ LawAssistant.Api/Controllers/AuthentificationController.cs
@@ -5,32 +5,41 @@ using LawAssistant.Application.Models;
 
 namespace LawAssistant.Api.Controllers
 {
-    [ApiController]
-    [Route("auth")]
+    [ApiController, Route("auth")]
     public class AuthentificationController(
         IAuthentificationService authService)
         : ControllerBase
     {
         [HttpPost("register")]
-        public async Task<IResult> RegisterAsync(RegisterRequest registerRequest)
+        public async Task<IActionResult> RegisterAsync(RegisterRequest registerRequest)
         {
             var registeredUser = await authService.RegisterAsync(registerRequest);
 
-            return registeredUser == null ? Results.BadRequest() : Results.Ok(registeredUser);
+            return registeredUser == null ? BadRequest() : Ok(registeredUser);
         }
 
         [HttpPost("login")]
-        public async Task<IResult> LoginAsync(LoginRequest loginRequest)
+        public async Task<IActionResult> LoginAsync(LoginRequest loginRequest)
         {
             var token = await authService.LoginAsync(loginRequest);
 
             if(token == null)
-                return Results.Unauthorized();
+                return Unauthorized();
 
-            var httpContext = ControllerContext.HttpContext;
+			HttpContext httpContext = ControllerContext.HttpContext;
             httpContext.Response.Cookies.Append("token", token);
 
-            return Results.Ok(token);
+            return Ok(token);
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+			HttpContext httpContext = ControllerContext.HttpContext;
+            httpContext.Response.Cookies.Delete("token");
+
+            return Ok();
+
         }
     }
 }
